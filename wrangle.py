2e00c246f464afc2611d0d1da8396e81d9348f6b
Brooke-Holyoak <brooke.holyoak24@gmail.com>
2021-09-07 12:24:52 -0500

    updating lesson and exercises

--- wrangle.py
+++ wrangle.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import os
+from sklearn.model_selection import train_test_split
 from env import host, user, password
 
 ###################### Acquire Telco Data ######################
@@ -52,3 +53,57 @@ def get_zillow_data():
         
     return df
 
+def clean_zillow_data(df):
+    '''
+    This function takes in the acquired df, from the new_zillow_data or get_zillow_data functions
+    and cleans up the data by dropping necessary rows and/or columns,
+    encoding and/or renaming columns, changing datatypes where necessary, etc. 
+    '''
+    #drop rows with null values
+    df = df.dropna()
+    
+    #Convert the fips columns from float to object since they are not numbers to be calculated, and the year and calculatedsquarefeet column
+    # from float to integer since it is not necessary to show decimal places
+    df = df.astype({"yearbuilt": int, "fips": object, "calculatedfinishedsquarefeet": int})
+    
+    # Create dummy columns for fips since each code represents a county
+    dummy_df = pd.get_dummies(df[['fips']], dummy_na=False)
+    
+    #concat on to the original df
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    #Change the column names to be the county names 
+    #According to FIPS codes, 06037 is Los Angeles County, 06059 is Orange County, and 06111 is Ventura County
+    #Rename columns for better readability and simplicity
+    df = df.rename(columns={"fips_6037.0": "la_county", "fips_6059.0": "orange_county", "fips_6111.0": "ventura_county", "bedroomcnt": "bedrooms", "bathroomcnt": "bathrooms", "calculatedfinishedsquarefeet": "total_sqft", "taxvaluedollarcnt": "tax_value", "yearbuilt": "year_built"})
+    
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    return train, validate, test
+
+#Handle outliers to see if shape of data changes at all
+#Using John Salas function:
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+    
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
+        
+    return df
+
+    
